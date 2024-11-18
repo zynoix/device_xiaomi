4298440c9e24a3b9fab4746dcbd6d224f13d0630
Marc Bourgoin <themard85@gmail.com>
2024-11-17 17:51:59 -0700

    sdm660-common: Setup ELF checks & regenerate vendor
    According to templates for py extract-utils
    
    https://github.com/LineageOS/android_tools_extract-utils/tree/lineage-22.0/templates
    Change-Id: Ibb1cef7c9492c091f1626f3c4623ad0cd3e7b708

--- extract-files.py
+++ extract-files.py
@@ -12,10 +12,35 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/xiaomi/sdm660-common',
+    'hardware/qcom-caf/sdm660',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+    (
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     ('system_ext/lib64/lib-imscamera.so', 'system_ext/lib64/lib-imsvideocodec.so'): blob_fixup()
@@ -35,7 +60,8 @@ module = ExtractUtilsModule(
     'sdm660-common',
     'xiaomi',
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 module.add_proprietary_file('proprietary-files-fm.txt').add_copy_files_guard(
