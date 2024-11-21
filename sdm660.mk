c1cc5c5459f24693c327132cff6b5315d0efee9b
Marc Bourgoin <themard85@gmail.com>
2024-11-21 13:24:16 -0700

    sdm660-common: Remove copy libutils-v33
    With enforced elf checks coming on A16, let's switch to the provided one from compat
    
    Change-Id: I193682ca769e902b7a8b2edd850ea14297a71dec

--- sdm660.mk
+++ sdm660.mk
@@ -441,8 +441,8 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.legacy
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+PRODUCT_PACKAGES += \
+    libutils-v33
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
