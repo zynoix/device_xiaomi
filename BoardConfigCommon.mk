9a16cb83fddb1c7185c863263da0d4591495128a
Michael Bestas <mkbestas@lineageos.org>
2024-12-01 22:30:22 +0200

    sdm660-common: gps: Convert remaining Android.mk to bp
    Change-Id: I75e6af92795fe755a48557aa06106406b14c6750

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,10 +85,6 @@ AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
 BOARD_HAS_QCA_FM_SOC := cherokee
 endif
 
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-LOC_HIDL_VERSION := 3.0
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
