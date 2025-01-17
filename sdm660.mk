f0c93cc69706bd7f3fff1c19659e39f14a0b91ab
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    sdm660-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- sdm660.mk
+++ sdm660.mk
@@ -291,7 +291,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
