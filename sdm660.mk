40d5d8d1fcaf229c64b6f87f8419779b6e900b69
Michael Bestas <mkbestas@lineageos.org>
2026-06-11 22:15:06 +0300

    sdm660-common: Switch to common libqti-perfd-client
    Change-Id: I652f80c03b26bd06586b9c035e4f28900cbf0d3d

--- sdm660.mk
+++ sdm660.mk
@@ -273,13 +273,10 @@ PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
     vendor_firmware_mnt_mountpoint
 
-# Perf
-PRODUCT_PACKAGES += \
-    libqti-perfd-client
-
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint_636.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_636.json \
@@ -287,7 +284,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Public libraries
 PRODUCT_COPY_FILES += \
