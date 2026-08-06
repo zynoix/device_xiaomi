0db6fb34679c6157de3f8b8eacfa31a7d688a797
zynoix <zynoix.io@gmail.com>
2026-08-06 16:19:43 +0530

    tulip: Remove Lineage Stuff

--- device.mk
+++ device.mk
@@ -278,8 +278,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
